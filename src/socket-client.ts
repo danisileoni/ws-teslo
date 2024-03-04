be633b13698c28ecfcd5ea7c3e18701beca65258
Daniele <danielesileoni4@gmail.com>
2024-03-03 21:07:13 -0300

    first commit

---
+++ src/socket-client.ts
@@ -0,0 +1,8 @@
+import {Manager} from 'socket.io-client';
+
+export const connectToServer = () => {
+  const manager = new Manager('localhost:3000/socket.io/socket.io.js');
+
+  const socket = manager.socket('/');
+  console.log(socket);
+}
