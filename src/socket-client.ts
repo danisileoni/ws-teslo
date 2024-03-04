95c87cf906af7c5c0c7501f49a0c4b86c79e050c
Daniele <danielesileoni4@gmail.com>
2024-03-03 21:29:27 -0300

    connection and disconnection the server

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,8 +1,21 @@
-import {Manager} from 'socket.io-client';
+import {Manager, Socket} from 'socket.io-client';
 
 export const connectToServer = () => {
   const manager = new Manager('localhost:3000/socket.io/socket.io.js');
 
   const socket = manager.socket('/');
-  console.log(socket);
+
+  addListeners(socket);
+}
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector('#server-status');
+
+  socket.on('connect', () => {
+    serverStatusLabel.innerHTML = 'connected';
+  });
+
+  socket.on('disconnect', () => {
+    serverStatusLabel.innerHTML = 'disconnected';
+  });
 }
