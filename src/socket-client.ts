c67f48caf5627ead95bf795dd8a48e069f7d8936
Daniele <danielesileoni4@gmail.com>
2024-03-03 21:52:01 -0300

    Clients connected

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -10,6 +10,7 @@ export const connectToServer = () => {
 
 const addListeners = (socket: Socket) => {
   const serverStatusLabel = document.querySelector('#server-status');
+  const clientsUl = document.querySelector('#clients-ul');
 
   socket.on('connect', () => {
     serverStatusLabel.innerHTML = 'connected';
@@ -18,4 +19,16 @@ const addListeners = (socket: Socket) => {
   socket.on('disconnect', () => {
     serverStatusLabel.innerHTML = 'disconnected';
   });
+
+  socket.on('clients-updated', (clients: string[]) => {
+    let clientsHtml = '';
+    clients.forEach(clientId => {
+      clientsHtml += `
+        <li>${clientId}</li>
+      `
+    })
+
+    clientsUl.innerHTML = clientsHtml;
+
+  })
 }
